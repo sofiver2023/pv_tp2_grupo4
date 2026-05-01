a54949844b8f04fe99c24347884bcd13df0af8c7
sofiver2023 <verdejasofia4@gmail.com>
2026-04-30 21:03:58 -0300

    Modificando mis ejercicios

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,22 +1,19 @@
-const gestionarRepuestos = () => {
-    const repuestos = ["Filtro de aceite", "Pastillas de freno", "Bujías", "Batería", "Amortiguadores", "Correa de distribución"];
-    
-    document.write(`<p>Cantidad total de categorías en stock: <b>${repuestos.length}</b></p>`);
-    
-    document.write("<h3>Listado de Repuestos:</h3>");
-    document.write("<ul>");
-    repuestos.forEach(item => {
-        document.write(`<li>${item}</li>`);
-    });
-    document.write("</ul>");
+const gestionarNumeros = () => {
+    const numeros = [];
+    let suma = 0;
 
-    const busqueda = prompt("Ingrese el nombre del repuesto que desea buscar en el taller:");
-    
-    if (repuestos.includes(busqueda)) {
-        document.write(`<p style='color: green;'>✅ El repuesto <b>${busqueda}</b> se encuentra en stock.</p>`);
-    } else {
-        document.write(`<p style='color: red;'>❌ El repuesto <b>${busqueda}</b> no está disponible actualmente.</p>`);
+    const cantidad = parseInt(prompt("¿Cuántos números desea ingresar?"));
+
+    for (let i = 0; i < cantidad; i++) {
+        let num = parseInt(prompt(`Ingrese el número entero ${i + 1} de ${cantidad}:`));
+        numeros.push(num);
+        suma += num;
     }
+
+    document.write(`<p>Números ingresados: <b>${numeros.join(", ")}</b></p>`);
+
+    const promedio = suma / numeros.length;
+    document.write(`<p>El promedio es: <b>${promedio}</b></p>`);
 };
 
-gestionarRepuestos();
+gestionarNumeros();
